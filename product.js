6ba3e81ed9a1ff304831cc54526e1e4c66df67c5
junaid2024 <muhammedjunaid2024@gmail.com>
2023-12-05 18:34:35 +0530

    final

---
+++ product.js
@@ -0,0 +1,51 @@
+
+
+
+function add() {
+    const product = {
+        id: document.getElementById("pid").value,
+        pname: document.getElementById("pname").value,
+        catg: document.getElementById("pcatagory").value,
+        description: document.getElementById("descrip").value,
+        price: document.getElementById("price").value,
+        review: document.getElementById("review").value
+    };
+
+    if (product.id in localStorage) {
+        alert("Already added");
+        document.getElementById("myForm").reset(); 
+
+    } else {
+        localStorage.setItem(product.id, JSON.stringify(product));
+        console.log(product);
+        alert('Added successfully');
+    }
+}
+
+
+
+
+
+function search() {
+    let searchid = document.getElementById('checkid').value;
+    var search = localStorage.getItem(searchid);
+    
+    if (search) {
+        var getdetails = JSON.parse(search);
+
+        console.log(search);
+        card.innerHTML = `<div class="card" style="width: 18rem;">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Product Name: ${getdetails.pname}</li>
+                <li class="list-group-item">Category: ${getdetails.catg}</li>
+                <li class="list-group-item">Description: ${getdetails.description}</li>
+                <li class="list-group-item">Price: ${getdetails.price}</li>
+                <li class="list-group-item">Review: ${getdetails.review}</li>
+            </ul>
+        </div>`;
+    } else {
+        alert('Product not found');
+    }
+}
+
+  
